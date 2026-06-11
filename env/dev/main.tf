a27dc050d37df050a1a4dd18c22c5e3cc35606aa
shubhdixitnoogler <shubhdixittab@gmail.com>
2026-06-11 00:48:27 +0000

    VM creation

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -8,3 +8,36 @@ module "storage_account" {
   st                   = var.st
   resource_group_names = module.resource_group.names
 }
+
+module "virtual_network" {
+  source               = "../../modules/virtual_network"
+  vnet                 = var.vnet
+  resource_group_names = module.resource_group.names
+}
+
+module "subnet" {
+  source                = "../../modules/subnet"
+  subnet                = var.subnet
+  resource_group_names  = module.resource_group.names
+  virtual_network_names = module.virtual_network.names
+}
+
+module "network_security_group" {
+  source               = "../../modules/network_security_group"
+  nsg                  = var.nsg
+  resource_group_names = module.resource_group.names
+}
+
+module "network_interface" {
+  source               = "../../modules/network_interface"
+  nic                  = var.nic
+  resource_group_names = module.resource_group.names
+  subnet_ids           = module.subnet.ids
+}
+
+module "virtual_machine" {
+  source               = "../../modules/virtual_machine"
+  vm                   = var.vm
+  resource_group_names = module.resource_group.names
+  nic_ids              = module.network_interface.ids
+}
--- env/dev/variable.tf
+++ env/dev/variable.tf
@@ -15,3 +15,75 @@ variable "st" {
     account_replication_type = string
   }))
 }
+
+variable "vnet" {
+  type = map(object({
+    name               = string
+    resource_group_key = string
+    location           = string
+    address_space      = list(string)
+  }))
+}
+
+variable "subnet" {
+  type = map(object({
+    name                = string
+    resource_group_key  = string
+    virtual_network_key = string
+    address_prefixes    = list(string)
+  }))
+}
+
+variable "nsg" {
+  type = map(object({
+    name               = string
+    resource_group_key = string
+    location           = string
+    security_rule = object({
+      name                       = string
+      priority                   = number
+      direction                  = string
+      access                     = string
+      protocol                   = string
+      source_port_range          = string
+      destination_port_range     = string
+      source_address_prefix      = string
+      destination_address_prefix = string
+    })
+  }))
+}
+
+variable "nic" {
+  type = map(object({
+    name               = string
+    resource_group_key = string
+    location           = string
+    ip_configuration = object({
+      name                          = string
+      subnet_key                    = string
+      private_ip_address_allocation = string
+    })
+  }))
+}
+
+variable "vm" {
+  type = map(object({
+    name               = string
+    resource_group_key = string
+    location           = string
+    size               = string
+    admin_username     = string
+    admin_password     = string
+    nic_key            = string
+    os_disk = object({
+      caching              = string
+      storage_account_type = string
+    })
+    source_image_reference = object({
+      publisher = string
+      offer     = string
+      sku       = string
+      version   = string
+    })
+  }))
+}
